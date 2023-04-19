8b13c5adce16815844da7a6a0234475111a8ff4b
Kornel Mrozowski <kpmrozowski@gmail.com>
2023-04-19 23:46:34 +0200

    added voleyball node

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. /opt/ros/noetic/setup.bash
+# merge all compile_commands.json recursively into one (for intellisense)
+jq -s 'map(.[])' build/**/compile_commands.json >> compile_commands.json
+# catkin_make -k0 -j2 -l2 --use-ninja -DCMAKE_C_COMPILER=/usr/bin/gcc-9 -DCMAKE_CXX_COMPILER=/usr/bin/g++-9 -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+catkin_make -k0 --use-ninja -DCMAKE_C_COMPILER=/usr/bin/gcc-9 -DCMAKE_CXX_COMPILER=/usr/bin/g++-9 -DCMAKE_EXPORT_COMPILE_COMMANDS=1
---
+++ src/comm_test/comm_test/src/volleyball_node.cpp
@@ -0,0 +1,68 @@
+#include <memory>
+#include <unistd.h>
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/builtin_uint64.h>
+
+#include <fmt/format.h>
+
+class VolleyballNode {
+    boost::function<void (const std_msgs::UInt64::ConstPtr&)> callback_jetson_;
+    boost::function<void (const std_msgs::UInt64::ConstPtr&)> callback_ground_;
+    ros::Subscriber sub_to_jetson_;
+    ros::Subscriber sub_to_ground_;
+    ros::Publisher pub_to_jetson_;
+    ros::Publisher pub_to_ground_;
+
+public:
+    VolleyballNode(ros::NodeHandle nh) {
+        callback_jetson_ = boost::function<void (const std_msgs::UInt64::ConstPtr&)>(
+            [this] (const std_msgs::UInt64::ConstPtr& msg_received) -> void {
+                ROS_INFO("I heard: [%zu] from jetson", msg_received->data);
+                std_msgs::UInt64 msg_to_send;
+                msg_to_send.data = msg_received->data + 1;
+                // usleep(5000);
+                pub_to_jetson_.publish(msg_to_send);
+            }
+        );
+        callback_ground_ = boost::function<void (const std_msgs::UInt64::ConstPtr&)>(
+            [this] (const std_msgs::UInt64::ConstPtr& msg_received) -> void {
+                ROS_INFO("I heard: [%zu] from ground", msg_received->data);
+                std_msgs::UInt64 msg_to_send;
+                msg_to_send.data = msg_received->data + 1;
+                // usleep(5000);
+                pub_to_ground_.publish(msg_to_send);
+            }
+        );
+        pub_to_jetson_ = nh.advertise<std_msgs::UInt64>("rpi_to_jetson", 1000);
+        pub_to_ground_ = nh.advertise<std_msgs::UInt64>("rpi_to_ground", 1000);
+        sub_to_jetson_ = nh.subscribe("jetson_to_rpi", 1000, callback_jetson_);
+        sub_to_ground_ = nh.subscribe("ground_to_rpi", 1000, callback_ground_);
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "player_rpi");
+    ros::NodeHandle nh;
+    // ros::NodeHandle nh_private("~");
+
+    VolleyballNode valley_ball_node(nh);
+
+    ros::Rate rate(24.);
+
+    while(ros::ok())
+    {
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+
+    ros::spinOnce();
+        rate.sleep();
+    }
+    printf("IT'S MILLER TIME! :D");
+    return 0;
+}
